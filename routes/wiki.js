90795dbbc521f20e92cfe377371c063243fd77c4
Nevin Chen <Nevinchen@yahoo.com>
2019-03-07 16:25:01 -0500

    linking post action with sequelize

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,13 +1,26 @@
 const express = require('express');
-const addPage = require('../views/addPage.js')
+const addPage = require('../views/addPage.js');
 const router = express.Router();
+const { Page } = require('../models');
 
 router.get('/', (req, res, next) => {
  res.send('wiki all');
 })
 
-router.post('/', (req, res, next) => {
-  res.send(req.body);
+router.post('/', async (req, res, next) => {
+
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  // try {
+  //   await page.save();
+  //   res.redirect('/');
+  // } catch (error) {
+  //   console.error(error);
+  // }
+  res.send(req.body.title);
 })
 
 router.get('/add', (req, res, next) => {
@@ -15,4 +28,3 @@ router.get('/add', (req, res, next) => {
 })
 
 module.exports = router
-
