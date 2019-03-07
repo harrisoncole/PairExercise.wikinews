4314f63563d561e93cd541dd8585de6b481b3970
Harrison Cole <harrison.d.cole@gmail.com>
2019-03-07 12:27:24 -0500

    added styles and db

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const morgan = require('morgan');
 const express = require('express');
 const path = require('path')
+const {db} = require('./models');
+const layout = require('./views/layout');
+
 
 const app = express();
 
 app.use(morgan('dev'));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use((req, res) => {
+    res.send(layout(''));
+});
 app.get('/', (req, res, next) => {
-   res.send('');
+   res.send('hello Nevin');
 })
 
 app.listen(2580, () => {
     console.log('Listening in on Port 2580!');
 })
+
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
+
